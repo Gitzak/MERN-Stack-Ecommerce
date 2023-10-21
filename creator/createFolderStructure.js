e993496e22d9fea5096f24874106d2413f6dfd2b
GitZak <zakaria.belrhali@gmail.com>
2023-10-21 09:56:58 +0100

    edit script cretor & generate structure

--- creator/createFolderStructure.js
+++ creator/createFolderStructure.js
@@ -22,30 +22,42 @@ function createFolderStructure() {
 
     const controllersPath = path.join(projectRoot, 'controllers');
     createDirectory(controllersPath);
+    createFile(path.join(controllersPath, 'userController.js'));
 
     const servicesPath = path.join(projectRoot, 'services');
     createDirectory(servicesPath);
+    createFile(path.join(servicesPath, 'userService.js'));
 
     const repositoriesPath = path.join(projectRoot, 'repositories');
     createDirectory(repositoriesPath);
+    createFile(path.join(repositoriesPath, 'userRepository.js'));
 
     const modelsPath = path.join(projectRoot, 'models');
     createDirectory(modelsPath);
+    createFile(path.join(modelsPath, 'User.js'));
 
     const routesPath = path.join(projectRoot, 'routes');
     createDirectory(routesPath);
-
+    createFile(path.join(routesPath, 'index.js'));
     const apiPath = path.join(routesPath, 'api');
     createDirectory(apiPath);
+    createFile(path.join(apiPath, 'userRoutes.js'));
 
     const middlewarePath = path.join(projectRoot, 'middleware');
     createDirectory(middlewarePath);
+    createFile(path.join(middlewarePath, 'authMiddleware.js'));
 
     const constantsPath = path.join(projectRoot, 'constants');
     createDirectory(constantsPath);
+    createFile(path.join(constantsPath, 'index.js'));
+
+    const configPath = path.join(projectRoot, 'config');
+    createDirectory(configPath);
+    createFile(path.join(configPath, 'keys.js'));
 
     const utilsPath = path.join(projectRoot, 'utils');
     createDirectory(utilsPath);
+    createFile(path.join(utilsPath, 'db.js'));
 
     // Create the additional files
     createFile(path.join(projectRoot, 'app.js'));
