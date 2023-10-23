610a0266e7945b5335c9842f4d691cd653ab7662
abdomallouk <abdelbassetmallouk@gmail.com>
2023-10-23 18:10:20 +0100

    adding user controllers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,28 +1,43 @@
-const { UserService } = require('../services/userService');
-const { UserRepository } = require('../repositories/userRepository')
-const User = require('../models/User');
+const { UserService } = require("../services/userService");
+const { UserRepository } = require("../repositories/userRepository");
+const User = require("../models/User");
 
 const userRepo = new UserRepository(User);
 const userServ = new UserService(userRepo);
 
-// Get user by ID
-const getUserById = async (req, res) => {
-    try {
-        const userId = req.params.id;
-        // const requestingUserRole = req.user.role;
-        const user = await userServ.getUserById(userId);
-        if(!user) {
-            return res.status(404).json({ message: 'User not found'});
-        }
-
-        return res.status(200).json({ status: 200, data: [user]});
-        // if (requestingUserRole === 'admin' || requestingUserRole === 'manager') {
-        // } else {
-        //     return res.status(403).json({ status: 403, message: "You don't have enough privilege"});
-        // }
-    } catch (error) {
-        res.status(500).json({ message: 'Server error'});
-    }
+const addNewUser = async (req, res) => {
+  const newUser = await userServ.AddUser(req);
+  // console.log('new user', newUser)
+  res.json(newUser);
 };
 
-module.exports = { getUserById };
+//update user data
+const updateUserData = async (req, res) => {
+  const updatedUser = await userServ.UpdateUser(req);
+  console.log(updatedUser);
+  res.json(updatedUser);
+};
+
+// Get user by ID
+const getUserById = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await userServ.getUserById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json({ status: 200, data: [user] });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+// delete user 
+const deleteUser = async (req, res) => {
+  const userId = req.params.id;
+  // console.log(userId);
+  const result = await userServ.Delete(userId);
+  res.json(result);
+};
+
+module.exports = { getUserById, addNewUser, updateUserData, deleteUser };
