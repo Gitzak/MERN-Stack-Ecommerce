d271f0e7865767aa7237fbda9c7ce425e7b1da9f
GitZak <zakaria.belrhali@gmail.com>
2023-10-27 16:04:09 +0100

    index route modif

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,25 +1,18 @@
 const router = require("express").Router();
 const userRoutes = require("./api/userRoutes");
-const categoryRoutes = require("./api/categoriesRoutes");
-// const subCategoriesRoutes = require("./api/subCategoriesRoutes");
-const subCategoriesRoutes = require("./api/subCategoriesRoutes")
 const customerRoutes = require("./api/customerRoutes");
+const categoryRoutes = require("./api/categoriesRoutes");
+const subCategoriesRoutes = require("./api/subCategoriesRoutes")
 const productRoutes = require("./api/productRoutes");
 const orderRoutes = require("./api/orderRoutes");
 
 // api Users routes
 router.use("/users", userRoutes);
 router.use("/customers", customerRoutes);
-<<<<<<< HEAD
 router.use("/categories", categoryRoutes);
-<<<<<<< HEAD
 router.use("/subcategories", subCategoriesRoutes);
-=======
-=======
 router.use("/products", productRoutes);
 router.use("/orders", orderRoutes);
->>>>>>> orders
->>>>>>> 8ad32b5beb2bf3439fc61eeda029a28bee44fa45
 
 router.use("*", (req, res) => res.status(404).json("No API route found"));
 
