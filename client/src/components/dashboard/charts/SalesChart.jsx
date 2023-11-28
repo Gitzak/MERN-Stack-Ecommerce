55c9060797c233767fff6137e1629c2ab615d36d
Yassir hakimi <yassirhakimi60@gmail.com>
2023-11-28 22:12:46 +0100

    client1

--- client/src/components/dashboard/charts/SalesChart.jsx
+++ client/src/components/dashboard/charts/SalesChart.jsx
@@ -1,32 +1,205 @@
 import * as React from "react";
 import { useTheme } from "@mui/material/styles";
-import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from "recharts";
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    Label,
+    ResponsiveContainer,
+} from "recharts";
 import Title from "../title/Title";
-
-// Generate Sales Data
-function createData(time, amount) {
-    return { time, amount };
-}
-
-const data = [createData("00:00", 0), createData("03:00", 300), createData("06:00", 600), createData("09:00", 800), createData("12:00", 1500), createData("15:00", 2000), createData("18:00", 2400), createData("21:00", 2400), createData("24:00", undefined)];
+import { useEffect } from "react";
+import { getAllOrders } from "../../../api/ordersApi";
+import { useState } from "react";
+import {
+    Button,
+    ButtonGroup,
+    ToggleButton,
+    ToggleButtonGroup,
+} from "@mui/material";
+import moment from "moment";
 
 export const SalesChart = () => {
+    const [orders, setOrders] = useState([]);
+    const [graphData, setGraphData] = useState([]);
+    const [timePeriod, setTimePeriod] = useState("Daily");
+    const [alignment, setAlignment] = useState("web");
+
+    const handleChange = (event, newAlignment) => {
+        setAlignment(newAlignment);
+    };
+
+    const buttons = [
+        <ToggleButton
+            value="one"
+            key="one"
+            onClick={() => setTimePeriod("Daily")}
+        >
+            Daily
+        </ToggleButton>,
+        <ToggleButton
+            value="two"
+            key="two"
+            onClick={() => setTimePeriod("Weekly")}
+        >
+            Weekly
+        </ToggleButton>,
+        <ToggleButton
+            value="three"
+            key="three"
+            onClick={() => setTimePeriod("Monthly")}
+        >
+            Monthly
+        </ToggleButton>,
+        <ToggleButton
+            value="four"
+            key="four"
+            onClick={() => setTimePeriod("Yearly")}
+        >
+            Yearly
+        </ToggleButton>,
+    ];
+
     const theme = useTheme();
+    const fetchOrders = async () => {
+        try {
+            const response = await getAllOrders();
+            setOrders(response.data.orders);
+        } catch (error) {
+            console.error("Error fetching orders:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchOrders();
+    }, []);
+
+    const groupDataByTimePeriod = (data, timePeriod) => {
+        return data.reduce((acc, item) => {
+            const date = moment(item.date);
+            let groupKey;
+
+            switch (timePeriod) {
+                case "Daily":
+                    groupKey = date.format("DD/MM/YYYY");
+                    break;
+                case "Weekly":
+                    groupKey = `Week ${date.week()} ${date.year()}`;
+                    break;
+                case "Monthly":
+                    groupKey = date.format("MMM YYYY");
+                    break;
+                case "Yearly":
+                    groupKey = date.format("YYYY");
+                    break;
+                default:
+                    groupKey = item.date;
+            }
+
+            if (!acc[groupKey]) {
+                acc[groupKey] = {
+                    date: groupKey,
+                    amount: 0,
+                };
+            }
+
+            acc[groupKey].amount += item.amount;
+            return acc;
+        }, {});
+    };
+
+    useEffect(() => {
+        try {
+            const sortedOrders = orders.sort(
+                (a, b) => a.orderDate - b.orderDate
+            );
+
+            const data = sortedOrders.map((order) => {
+                return {
+                    date: new Date(order.orderDate).toLocaleDateString(),
+                    amount: order.cartTotalPrice,
+                };
+            });
+
+            // const data = orders.map((order) => {
+            //     return {
+            //         date: new Date(order.orderDate).toLocaleDateString(),
+            //         amount: order.cartTotalPrice,
+            //     };
+            // });
+
+            const groupedData = groupDataByTimePeriod(data, timePeriod);
+            setGraphData(Object.values(groupedData));
+        } catch (error) {
+            console.error("Error fetching orders:", error);
+        }
+    }, [orders, timePeriod]);
+
+    const formatXAxis = (tickItem) => {
+        switch (timePeriod) {
+            case "Daily":
+                return moment(tickItem, "DD/MM/YYYY").format("DD/MM");
+            case "Weekly":
+                // Extract week number from the string
+                const weekNumber = tickItem.split(" ")[1];
+                return `Week ${weekNumber}`;
+            case "Monthly":
+                return moment(tickItem, "MMM YYYY").format("MMM YYYY");
+            case "Yearly":
+                return moment(tickItem, "YYYY").format("YYYY");
+            default:
+                return tickItem;
+        }
+    };
+
+    const xAxisMin = Math.min(
+        ...graphData.map((item) => new Date(item.date).getTime())
+    );
+    const xAxisMax = Math.max(
+        ...graphData.map((item) => new Date(item.date).getTime())
+    );
+
+    const yAxisMin = Math.min(...graphData.map((item) => item.amount));
+    const yAxisMax = Math.max(...graphData.map((item) => item.amount));
 
     return (
         <React.Fragment>
             <Title>Sales DATA</Title>
+
+            <ToggleButtonGroup
+                color="primary"
+                value={alignment}
+                exclusive
+                onChange={handleChange}
+                aria-label="Platform"
+                sx={{ alignSelf: "center" }}
+            >
+                {buttons}
+            </ToggleButtonGroup>
             <ResponsiveContainer>
                 <LineChart
-                    data={data}
+                    data={graphData}
                     margin={{
                         top: 16,
                         right: 16,
                         bottom: 0,
                         left: 24,
-                    }}>
-                    <XAxis dataKey="time" stroke={theme.palette.text.secondary} style={theme.typography.body2} />
-                    <YAxis stroke={theme.palette.text.secondary} style={theme.typography.body2}>
+                    }}
+                >
+                    <XAxis
+                        dataKey="date"
+                        stroke={theme.palette.text.secondary}
+                        style={theme.typography.body2}
+                        tickFormatter={formatXAxis}
+                        domain={[xAxisMin, xAxisMax]}
+                    />
+
+                    <YAxis
+                        stroke={theme.palette.text.secondary}
+                        style={theme.typography.body2}
+                        domain={[yAxisMin, yAxisMax]}
+                    >
                         <Label
                             angle={270}
                             position="left"
@@ -34,13 +207,21 @@ export const SalesChart = () => {
                                 textAnchor: "middle",
                                 fill: theme.palette.text.primary,
                                 ...theme.typography.body1,
-                            }}>
+                            }}
+                        >
                             Sales ($)
                         </Label>
                     </YAxis>
-                    <Line isAnimationActive={false} type="monotone" dataKey="amount" stroke={theme.palette.primary.main} dot={false} />
+                    <Line
+                        isAnimationActive={false}
+                        type="monotone"
+                        dataKey="amount"
+                        stroke={theme.palette.primary.main}
+                        dot={false}
+                    />
                 </LineChart>
             </ResponsiveContainer>
+            {console.log(graphData)}
         </React.Fragment>
     );
 };
--- client/src/components/dashboard/tables/LatestOrders.jsx
+++ client/src/components/dashboard/tables/LatestOrders.jsx
@@ -1,4 +1,64 @@
+// import * as React from "react";
+// import Link from "@mui/material/Link";
+// import Table from "@mui/material/Table";
+// import TableBody from "@mui/material/TableBody";
+// import TableCell from "@mui/material/TableCell";
+// import TableHead from "@mui/material/TableHead";
+// import TableRow from "@mui/material/TableRow";
+// import Title from "../title/Title";
+
+// // Generate Order Data
+// function createData(id, date, name, shipTo, paymentMethod, amount) {
+//     return { id, date, name, shipTo, paymentMethod, amount };
+// }
+
+// const rows = [
+//     createData(0, "16 Mar, 2019", "Elvis Presley", "Tupelo, MS", "VISA ⠀•••• 3719", 312.44),
+//     createData(1, "16 Mar, 2019", "Paul McCartney", "London, UK", "VISA ⠀•••• 2574", 866.99),
+//     createData(2, "16 Mar, 2019", "Tom Scholz", "Boston, MA", "MC ⠀•••• 1253", 100.81),
+//     createData(3, "16 Mar, 2019", "Michael Jackson", "Gary, IN", "AMEX ⠀•••• 2000", 654.39),
+//     createData(4, "15 Mar, 2019", "Bruce Springsteen", "Long Branch, NJ", "VISA ⠀•••• 5919", 212.79),
+// ];
+
+// function preventDefault(event) {
+//     event.preventDefault();
+// }
+
+// export default function LatestOrders() {
+//     return (
+//         <React.Fragment>
+//             <Title>Recent Orders</Title>
+//             <Table size="small">
+//                 <TableHead>
+//                     <TableRow>
+//                         <TableCell>Date</TableCell>
+//                         <TableCell>Name</TableCell>
+//                         <TableCell>Ship To</TableCell>
+//                         <TableCell>Payment Method</TableCell>
+//                         <TableCell align="right">Sale Amount</TableCell>
+//                     </TableRow>
+//                 </TableHead>
+//                 <TableBody>
+//                     {rows.map((row) => (
+//                         <TableRow key={row.id}>
+//                             <TableCell>{row.date}</TableCell>
+//                             <TableCell>{row.name}</TableCell>
+//                             <TableCell>{row.shipTo}</TableCell>
+//                             <TableCell>{row.paymentMethod}</TableCell>
+//                             <TableCell align="right">{`$${row.amount}`}</TableCell>
+//                         </TableRow>
+//                     ))}
+//                 </TableBody>
+//             </Table>
+//             <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+//                 See more orders
+//             </Link>
+//         </React.Fragment>
+//     );
+// }
+
 import * as React from "react";
+import { useState, useEffect } from "react";
 import Link from "@mui/material/Link";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -6,51 +66,75 @@ import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Title from "../title/Title";
+import { getAllOrders } from "../../../api/ordersApi";
 
 // Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-    return { id, date, name, shipTo, paymentMethod, amount };
+function createData(id, date, name, amount) {
+    return { id, date, name, amount };
 }
 
-const rows = [
-    createData(0, "16 Mar, 2019", "Elvis Presley", "Tupelo, MS", "VISA ⠀•••• 3719", 312.44),
-    createData(1, "16 Mar, 2019", "Paul McCartney", "London, UK", "VISA ⠀•••• 2574", 866.99),
-    createData(2, "16 Mar, 2019", "Tom Scholz", "Boston, MA", "MC ⠀•••• 1253", 100.81),
-    createData(3, "16 Mar, 2019", "Michael Jackson", "Gary, IN", "AMEX ⠀•••• 2000", 654.39),
-    createData(4, "15 Mar, 2019", "Bruce Springsteen", "Long Branch, NJ", "VISA ⠀•••• 5919", 212.79),
-];
-
-function preventDefault(event) {
-    event.preventDefault();
-}
+// function preventDefault(event) {
+//     event.preventDefault();
+// }
 
 export default function LatestOrders() {
+    const [rows, setRows] = useState([]);
+    const [orders, setOrders] = useState([]);
+
+    const fetchOrders = async () => {
+        try {
+            const response = await getAllOrders();
+            setOrders(response.data.orders);
+            console.log(orders);
+            // const data = orders.map((order, index) => {
+            //     return createData(
+            //         index,
+            //         new Date(order.orderDate).toLocaleDateString(),
+            //         order.customerFirstName,
+            //         order.cartTotalPrice
+            //     );
+            // });
+
+            // setRows(data);
+            // console.log("rows", rows);
+        } catch (error) {
+            console.error("Error fetching orders:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchOrders();
+    }, []);
+
     return (
         <React.Fragment>
             <Title>Recent Orders</Title>
             <Table size="small">
                 <TableHead>
                     <TableRow>
+                        <TableCell>Order Number</TableCell>
                         <TableCell>Date</TableCell>
                         <TableCell>Name</TableCell>
-                        <TableCell>Ship To</TableCell>
-                        <TableCell>Payment Method</TableCell>
                         <TableCell align="right">Sale Amount</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map((row) => (
-                        <TableRow key={row.id}>
-                            <TableCell>{row.date}</TableCell>
-                            <TableCell>{row.name}</TableCell>
-                            <TableCell>{row.shipTo}</TableCell>
-                            <TableCell>{row.paymentMethod}</TableCell>
-                            <TableCell align="right">{`$${row.amount}`}</TableCell>
+                    {orders.slice(0, 5).map((row) => (
+                        <TableRow key={row._id}>
+                            <TableCell>{ `#${row.orderNumber.toString().padStart(6, "0")}`}</TableCell>
+                            <TableCell>{new Date(row.orderDate).toLocaleDateString()}</TableCell>
+                            <TableCell>{row.customerFirstName}</TableCell>
+                            <TableCell align="right">{`$${row.cartTotalPrice}`}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
-            <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+            <Link
+                color="primary"
+                href="#"
+                // onClick={preventDefault}
+                sx={{ mt: 3 }}
+            >
                 See more orders
             </Link>
         </React.Fragment>
